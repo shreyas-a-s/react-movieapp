56bfbb67ad728401083d46b19dbdf41bec7385a9
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-09 17:05:32 +0530

    feat: Create movie card

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?apikey=bdfab4f8"
 
+const movie_one = {
+  "Title": "The Avengers",
+  "Year": "2012",
+  "imdbID": "tt0848228",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+}
+
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -28,6 +36,26 @@ const App = () => {
           src={SearchIcon}
           alt="search icon"
         />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie_one.Year}</p>
+          </div>
+
+          <div>
+            <img
+              src={ movie_one.Poster !== 'N/A' ? movie_one.Poster : 'https://via.placeholder.com/400'}
+              alt={movie_one.Title}
+            />
+          </div>
+
+          <div>
+            <span>{movie_one.Type}</span>
+            <h3>{movie_one.Title}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
