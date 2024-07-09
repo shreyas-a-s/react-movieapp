ba5a16628fcbcb9d5a65e1fd531383faaedfa1d6
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-09 17:29:12 +0530

    refactor: Rename variable movie_one -> movie

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
             <div className="container">
               {
                 movies.map((movie) => (
-                  <MovieCard movie_one={movie} />
+                  <MovieCard movie={movie} />
                 ))
               }
             </div>
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,22 +1,22 @@
 import React from "react"
 
-const MovieCard = ({ movie_one }) => {
+const MovieCard = ({ movie }) => {
   return (
     <div className="movie">
       <div>
-        <p>{movie_one.Year}</p>
+        <p>{movie.Year}</p>
       </div>
 
       <div>
         <img
-          src={ movie_one.Poster !== 'N/A' ? movie_one.Poster : 'https://via.placeholder.com/400'}
-          alt={movie_one.Title}
+          src={ movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'}
+          alt={movie.Title}
         />
       </div>
 
       <div>
-        <span>{movie_one.Type}</span>
-        <h3>{movie_one.Title}</h3>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
       </div>
     </div>
   )
