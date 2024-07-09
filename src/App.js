1a3fd53ee3daab6efb943fa4e5de870097b3b6cc
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-09 15:52:09 +0530

    feat: Use OMDb API to return movie list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com/?apikey=bdfab4f8"
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
+
   return (
     <h1>App</h1>
   );
