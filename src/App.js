9fe7d47d5374d0162a4b0a7e7857327af7140cd2
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-09 17:34:06 +0530

    feat: Make searchbox functional

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const API_URL = "http://www.omdbapi.com/?apikey=bdfab4f8"
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -28,10 +29,13 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt="search icon"
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
